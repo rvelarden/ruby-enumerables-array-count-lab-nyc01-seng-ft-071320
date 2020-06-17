835831658b0acbf151d09a4faae3fc7d8d88d339
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-17 19:48:28 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,16 @@
+
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+string = ""  
+array.count do
+  |string|
+  end 
 end
+ 
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+array.count do
+  |string|
+    string = ""
+  end
 end
